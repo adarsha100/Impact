550946310331e2058a853246824bfc4924c675fe
Patrick Kenney <patrickkenney9801@gmail.com>
2018-09-15 16:19:45 -0500

    v0.0.1 dirt generated on start

---
+++ Assets/Scripts/startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startup : MonoBehaviour {
+
+    public Transform ground;
+
+    // called once before Start()
+    void Awake()
+    {
+        for (int i = 0; i < 40; i++)
+            for (int j = 0; j < 50; j++)
+                Instantiate(ground, new Vector3(i*4, -j*4, 0), Quaternion.identity);
+    }
+}
